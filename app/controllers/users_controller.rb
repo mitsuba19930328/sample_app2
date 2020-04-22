5bc76edebbdc731f080ca02928f210ce4618007f
mitsuba19930328 <mitsuba19930328@gmail.com>
2020-04-22 13:22:52 +0000

    before 7.3.4 失敗時のテスト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,23 @@ class UsersController < ApplicationController
   end
   
   def new
+    @user = User.new
   end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # 成功時の処理を行う
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 
 end
