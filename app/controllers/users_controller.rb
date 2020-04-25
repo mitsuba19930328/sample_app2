d214b7d2826c9de8cf7f63b47699e55d57afca3a
mitsuba19930328 <mitsuba19930328@gmail.com>
2020-04-25 08:27:11 +0000

    Finish sign-up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # 成功時の処理を行う
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
