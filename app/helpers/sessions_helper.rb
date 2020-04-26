78d2177857058fcb037b2026beed44c5fa7e7013
mitsuba19930328 <mitsuba19930328@gmail.com>
2020-04-26 08:44:27 +0000

    before 8.24 test green

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,8 +5,12 @@ module SessionsHelper
   end
   
   def current_user
-    if sesion[:user_id]
+    if session[:user_id]
       @current_user ||= User.find_by(id: session[:user_id])
     end
+  end
+  
+  def logged_in?
+    !current_user.nil?
   end
 end
