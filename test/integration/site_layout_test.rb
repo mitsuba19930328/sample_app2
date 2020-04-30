37ed59e8063df764a23eaedd9ea8d940ce61b023
mitsuba19930328 <mitsuba19930328@gmail.com>
2020-04-30 02:10:11 +0000

    before 10.3.3 ページネーション

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -11,4 +11,22 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]",signup_path
   end
+  
+  
+  def setup
+    @user = users(:michael)
+  end
+
+  test "layout links after logged in user" do
+    log_in_as(@user) #userでログイン
+    get root_path #root_path
+    assert_template 'static_pages/home' #ホームにたどり着いてるかチェック
+    assert_select "a[href=?]", root_path, count: 2 #ホームへのアクセスチェック
+    assert_select "a[href=?]", help_path #ヘルプへのアクセスチェック
+    assert_select "a[href=?]", users_path #ユーザー一覧へのアクセスチェック
+    assert_select "a[href=?]", user_path(@user) #ユーザー詳細ページへのチェック
+    assert_select "a[href=?]", edit_user_path(@user) #特定のユーザーアップデートページへのチェック
+    assert_select "a[href=?]", logout_path #ログアウト
+  end
+  
 end
