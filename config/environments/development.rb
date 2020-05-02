c0d5f5d8303b6ee204cf5e7847a3c28abe569f9e
mitsuba19930328 <mitsuba19930328@gmail.com>
2020-05-02 12:49:45 +0000

    before 11.2.3 送信メールのテスト

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,9 +27,8 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.perform_caching = false
+  # config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.perform_caching = false
   
   # railsでmailerいじる時に追加
   config.action_mailer.raise_delivery_errors = true
