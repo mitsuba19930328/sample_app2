5b6c4e1d95507907f121285c51277641e5477ad1
mitsuba19930328 <mitsuba19930328@gmail.com>
2020-05-03 07:28:47 +0000

    リスト 11.34: not green

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -28,6 +28,10 @@ Rails.application.configure do
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
   config.action_mailer.perform_caching = false
+  
+  # railsチュート11章で追加（リスト11.21）
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: 'example.com' }
 
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
