073cbf52747b697f30c7c77a6b8ec09597be8480
mitsuba19930328 <mitsuba19930328@gmail.com>
2020-05-04 12:35:10 +0000

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,4 +89,20 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  # railsチュートリアル11章　リスト 11.41
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'https://thawing-retreat-67235.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+  
 end
