8ebee3524776ec6231800b223a73f55c61c7cf4e
mitsuba19930328 <mitsuba19930328@gmail.com>
2020-05-05 05:20:29 +0000

    before 12.3 パスワードを再設定する

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -12,6 +12,8 @@ class UserMailerPreview < ActionMailer::Preview
   # Preview this email at
   # https://0a40516caf78418495510c44a3ce42e3.vfs.cloud9.us-east-2.amazonaws.com/rails/mailers/user_mailer/password_reset
   def password_reset
-    UserMailer.password_reset
+    user = User.first
+    user.reset_token = User.new_token
+    UserMailer.password_reset(user)
   end
 end
